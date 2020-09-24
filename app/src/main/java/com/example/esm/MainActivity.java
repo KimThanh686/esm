6cff84775f15001c9361211040b0b0a278a20732
TanDo <tan.do.coder@gmail.com>
2020-09-24 21:27:57 +0700

    first commit

---
+++ app/src/main/java/com/example/esm/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.esm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtuser, edtpassword;
+    TextView txtforgetpw;
+    Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AnhXa();
+
+        txtforgetpw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private void AnhXa() {
+        txtforgetpw = (TextView)findViewById(R.id.forgetpw);
+    }
+
+}
